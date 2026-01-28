b4792370e948015642d6f575b74a7f3456a35098
ronaksharma-simform <ronak.sharma@simformsolutions.com>
2026-01-28 15:24:25 +0530

    TE-T101 Added helper function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Hello World !!")
 
 const feature_T101="This is code for thew task specified in ZOHO TASK ID TE-T101"
+const feature_T101_1="This is the helper function for task feature T101"
